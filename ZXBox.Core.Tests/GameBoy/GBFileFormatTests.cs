a490243337d52b14c70df67a5b92434ee1a34aea
Jimmy Engström <jimmy@apeoholic.se>
2022-07-12 17:18:42 +0200

    Added very early stage of .Tap support

--- ZXBox.Core.Tests/GameBoy/GBFileFormatTests.cs
+++ ZXBox.Core.Tests/GameBoy/GBFileFormatTests.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.IO;
 using ZXBox.Snapshot;
 
 namespace ZXBox.Core.Tests.GameBoy;
 
 [TestClass]
-public class UnitTests
+public class GBFileFormatTests
 {
     [TestMethod]
     public void TestLoadGBFile()
@@ -13,5 +14,22 @@ public class UnitTests
         var bytes = File.ReadAllBytes(@"C:\Code\Roms\Test.gb");
         var ff = new GBFileFormat();
         ff.LoadSnapshot(bytes, new ZXSpectrum());
+    }
+
+    [TestMethod]
+    public void LoadTileTest()
+    {
+        var bytes = File.ReadAllBytes(@"C:\Code\Roms\Test.gb");
+        var ff = new GBFileFormat();
+        var gb = new Gameboy();
+        ff.LoadSnapshot(bytes, gb);
+        List<int> tiles = new List<int>();
+        //gb.PC = 0x100; //Entry point
+        gb.DoIntructions(1000000000);
+        for (int b = 0x8000; b <= 0x97FF; b++)
+        {
+            tiles.Add(gb.ReadByteFromMemory(b));
+        }
+
     }
 }
---
+++ ZXBox.Core/Cpus/Sharp/LR35902.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Zilog;
+
+namespace ZXBox.Core.Cpus.Sharp
+{
+    public class LR35902 : Z80
+    {
+        public override int ReadByteFromMemory(int address)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void WriteByteToMemory(int address, int bytetowrite)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void WriteWordToMemory(int address, int word)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        //This is implemented base on information found here https://gbdev.io/pandocs/CPU_Comparison_with_Z80.html
+        public override void DoIntructions(int numberOfTStates, Func<Z80, int> gameSpecificFunc)
+        {
+            NumberOfTstates = numberOfTStates;
+            NumberOfTStatesLeft += numberOfTStates;
+            _EndTstates2 = numberOfTStates;
+            while (true)
+            {
+                if (interruptTriggered(NumberOfTStatesLeft))
+                {
+                    NumberOfTStatesLeft -= interrupt();
+                    break;
+                }
+
+                //Refresh(1);
+
+                NextOpcode();
+                if (gameSpecificFunc != null)
+                {
+                    NumberOfTStatesLeft -= gameSpecificFunc(this);
+                }
+
+                switch (opcode)
+                {
+                    case 0xCB:
+                        NextOpcode();
+                        DoCBPrefixInstruction();
+                        break;
+                    case 0xDD:
+                        //Removed
+                        break;
+                    case 0xED:
+                        Refresh(1);
+                        NextOpcode();
+                        DoEDPrefixInstruction();
+                        break;
+                    case 0xFD:
+                        //Removed
+                        break;
+                    default:
+                        Refresh(1);
+                        switch (opcode)
+                        {
+                            case 0x08:
+                                WriteByteToMemory(GetNextPCWord(), SP); NumberOfTStatesLeft -= 13;
+                                break;
+                            case 0x10://STOP
+                                Halt();
+                                break;
+                            case 0x22://LDI(HL),A
+                                WriteByteToMemory(HL, A);
+                                HL++;
+                                break;
+                            case 0x2A://LDI  A,(HL)
+                                A = ReadByteFromMemory(HL);
+                                HL++;
+                                break;
+                            case 0x32://LDD(HL),A
+                                WriteByteToMemory(HL, A);
+                                HL--;
+                                break;
+                            case 0x3A://LDD  A,(HL)
+                                A = ReadByteFromMemory(HL);
+                                HL--;
+                                break;
+                            case 0xD3:// -
+                                break;
+                            case 0xD9://RETI
+                                RET(true, 14, 0);
+                                break;
+                            case 0xDB://-
+                                break;
+                            case 0xE0://LD(FF00 + n),A
+                                WriteByteToMemory(0xFF00 + GetNextPCByte(), A);
+                                break;
+                            case 0xE2://LD(FF00 + C),A
+                                WriteByteToMemory(0xFF00 + C, A);
+                                break;
+                            case 0xE3://-
+                                break;
+                            case 0xE4://-
+                                break;
+                            case 0xE8://ADD SP, dd
+
+                                break;
+                            case 0xEA://LD(nn),A
+                                WriteByteToMemory(GetNextPCWord(), A); NumberOfTStatesLeft -= 13;
+                                break;
+                            case 0xEB://-
+                                break;
+                            case 0xEC://-
+                                break;
+                            case 0xED://-
+                                break;
+                            case 0xF0://LD A,(FF00 + n)
+                                A = ReadByteFromMemory(0xFF00 + GetNextPCByte());
+                                NumberOfTStatesLeft -= 7;
+                                break;
+                            case 0xF2://LD   A,(FF00 + C)
+                                A = ReadByteFromMemory(0xFF00 + C);
+                                NumberOfTStatesLeft -= 7;
+                                break;
+                            case 0xF4://-
+                                break;
+                            case 0xF8://LD HL, SP+dd
+                                break;
+                            case 0xFA: //LD   A,(nn)
+                                A = ReadByteFromMemory(GetNextPCWord());
+                                NumberOfTStatesLeft -= 7;
+                                break;
+                            case 0xFC:// -
+                                break;
+                        }
+                        DoNoPrefixInstruction();
+                        break;
+                }
+            }
+        }
+    }
+}
